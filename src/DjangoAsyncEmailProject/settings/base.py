f0addc41b9b259660c80133993076fd0cc22eca5
Jayesh <jayesh@virtualinit.net>
2022-02-13 15:23:16 +0530

    update

--- src/DjangoAsyncEmailProject/settings/base.py
+++ src/DjangoAsyncEmailProject/settings/base.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*&3+1bjl0pup%m(5&_q#2w^f1xr%4aa@0ek)ypblj-dvn*yp7x'
+SECRET_KEY = ')^4*12hkl8whw$m)5&%u_2w^f8qn%4bm@8ee)byhwk-lfn#yp8a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
